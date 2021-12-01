253904ab0109e70f934d876fc874ed727a5d6db8
Raju Sahu <43608832+Raju1822@users.noreply.github.com>
2021-12-01 21:51:14 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'intern_db',
+	'hostname' => 'sql6.freesqldatabase.com',
+	'username' => 'sql6455636',
+	'password' => 'ZDnCSBan17',
+	'database' => 'sql6455636',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
